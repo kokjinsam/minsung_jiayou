816a1079b321aa910ba526e0fd2a990497dd2429
Kok J Sam <samkj.ks@gmail.com>
2017-11-28 20:12:26 -0500

    add data

---
+++ imports/api/data.js
@@ -0,0 +1,331 @@
+export const data = {
+  user: {
+    username: "kms1557",
+    member_since: "2017-08-22T18:38:19.169Z",
+    total_score: 74540,
+    avatar:
+      "http://gravatar.com/avatar/9ad8344285ffc90c3fa2e951a974e1d0.jpg?s=80\u0026r=pg",
+  },
+  courses: {
+    completed: [
+      {
+        title: "JavaScript Road Trip Part 1",
+        url: "http://www.codeschool.com/courses/javascript-road-trip-part-1",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/296/completed-javascript-road-trip-part-1-b9f5af5196fb596271f7f97b6b477d24.png",
+      },
+      {
+        title: "Front-end Formations",
+        url: "http://www.codeschool.com/courses/front-end-formations",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/315/completed-front-end-formations-165329e44a56cd6d401c0c60369d368e.png",
+      },
+      {
+        title: "Front-end Foundations",
+        url: "http://www.codeschool.com/courses/front-end-foundations",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/448/course-complete-81fb30908f005de5c75e5376d041f05f.png",
+      },
+      {
+        title: "The Elements of Web Design",
+        url: "http://www.codeschool.com/courses/the-elements-of-web-design",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/635/completed-the-elements-of-web-design-c6915faeadf0e4d230f10419b05eb3d7.png",
+      },
+      {
+        title: "The Magical Marvels of MongoDB",
+        url: "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/539/the-magical-marvels-of-mongodb-1f09c10dfafb300ce53d2d339fdc299c.png",
+      },
+      {
+        title: "The Sequel to SQL",
+        url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/518/course-complete-61a5b8d40f49efccf26fd3e84619545b.png",
+      },
+      {
+        title: "Try SQL",
+        url: "http://www.codeschool.com/courses/try-sql",
+        badge:
+          "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/490/try-sql-2eeaad2f3a3ad35a5fed7b63563ec9b8.png",
+      },
+    ],
+    in_progress: [],
+  },
+  badges: [
+    {
+      name: "Completed JavaScript Road Trip Part 1",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/296/completed-javascript-road-trip-part-1-b9f5af5196fb596271f7f97b6b477d24.png",
+      course_url:
+        "http://www.codeschool.com/courses/javascript-road-trip-part-1",
+    },
+    {
+      name: "Level 2 on JavaScript Road Trip Part 1",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/298/level-2-on-javascript-road-trip-part-1-e568b2c31b1fd0da0dc94cc4d5452150.png",
+      course_url:
+        "http://www.codeschool.com/courses/javascript-road-trip-part-1",
+    },
+    {
+      name: "Level 3 on JavaScript Road Trip Part 1",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/299/level-3-on-javascript-road-trip-part-1-ae7652ab8f363e27fea8afe06cee30e3.png",
+      course_url:
+        "http://www.codeschool.com/courses/javascript-road-trip-part-1",
+    },
+    {
+      name: "Level 1 on JavaScript Road Trip Part 1",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/297/level-1-on-javascript-road-trip-part-1-ff645bd94243922a90b775da5ee2647c.png",
+      course_url:
+        "http://www.codeschool.com/courses/javascript-road-trip-part-1",
+    },
+    {
+      name: "Completed Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/315/completed-front-end-formations-165329e44a56cd6d401c0c60369d368e.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Level 5 on Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/320/level-5-on-front-end-formations-cb3cef3fb878f727936622823c99698d.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Level 4 on Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/319/level-4-on-front-end-formations-f76933da76d45889cd6441de1987413f.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Journeyperson",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/497/journeyperson-7d6eea0f0d15e9c2fed170689934b4b4.png",
+      course_url: null,
+    },
+    {
+      name: "Level 3 on Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/318/level-3-on-front-end-formations-5db12952d80199300bd5106e47f84b44.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Level 2 on Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/317/level-2-on-front-end-formations-f8f7a7c43e94c88a4e2631386777fc73.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Level 1 on Front-end Formations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/316/level-1-on-front-end-formations-c27483b64148275c96bb8d0c999893d9.png",
+      course_url: "http://www.codeschool.com/courses/front-end-formations",
+    },
+    {
+      name: "Course Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/448/course-complete-81fb30908f005de5c75e5376d041f05f.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 5 Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/449/level-5-complete-854f2231143a3b3b49bc44c00cbbe7e7.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 4 Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/447/level-4-complete-161720168d30cd46936555dd6f4e20ef.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 1 Complete on Front End Foundations",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/444/level-1-complete-on-front-end-foundations-a14a15c153bbe32611fca5be835923bf.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 3 Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/446/level-3-complete-3ce06a25d49e2457712ba2b72463a66c.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 2 Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/445/level-2-complete-8189525329228c8f6513e22202b8729b.png",
+      course_url: "http://www.codeschool.com/courses/front-end-foundations",
+    },
+    {
+      name: "Level 4 on The Elements of Web Design",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/634/level-4-on-the-elements-of-web-design-415b0973319201b230ca5308d3d5b845.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-elements-of-web-design",
+    },
+    {
+      name: "Completed The Elements of Web Design",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/635/completed-the-elements-of-web-design-c6915faeadf0e4d230f10419b05eb3d7.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-elements-of-web-design",
+    },
+    {
+      name: "Level 3 on The Elements of Web Design",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/633/level-3-on-the-elements-of-web-design-197cc52dc8318d5731ce19e4700a2240.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-elements-of-web-design",
+    },
+    {
+      name: "Apprentice",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/534/apprentice-0f19184c0b7fc1eaa3e9c89e758c5d99.png",
+      course_url: null,
+    },
+    {
+      name: "Level 2 on The Elements of Web Design",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/632/level-2-on-the-elements-of-web-design-ec65f72dc9047b7844b8c3834adeb7dc.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-elements-of-web-design",
+    },
+    {
+      name: "Level 1 on The Elements of Web Design",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/631/level-1-on-the-elements-of-web-design-c0e859c6119520c52189065e6571b38a.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-elements-of-web-design",
+    },
+    {
+      name: "The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/539/the-magical-marvels-of-mongodb-1f09c10dfafb300ce53d2d339fdc299c.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Level 5 on The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/544/level-5-on-the-magical-marvels-of-mongodb-d34095c05b9158d04799fc233a79a06e.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Level 4 on The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/543/level-4-on-the-magical-marvels-of-mongodb-3217775000ecd658752e864950bef9b8.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Level 3 on The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/542/level-3-on-the-magical-marvels-of-mongodb-b02d1008286c4049fce687c0d4432685.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Tenderfoot",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/496/tenderfoot-59d41c72e7dafb6adafe6802e288d636.png",
+      course_url: null,
+    },
+    {
+      name: "Level 2 on The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/541/level-2-on-the-magical-marvels-of-mongodb-9666be0ee1083fa52e7a7a345ce443f4.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Level 1 on The Magical Marvels of MongoDB",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/540/level-1-on-the-magical-marvels-of-mongodb-b8ff6f0cdcaed6b7cfdbb71087230fa6.png",
+      course_url:
+        "http://www.codeschool.com/courses/the-magical-marvels-of-mongodb",
+    },
+    {
+      name: "Level 5 on The Sequel to SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/517/level-5-on-the-sequel-to-sql-6e2f151d694196ed6df3435caf404201.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Course Complete",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/518/course-complete-61a5b8d40f49efccf26fd3e84619545b.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Level 4 on The Sequel to SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/516/level-4-on-the-sequel-to-sql-ad0777053c3fc2cb4729f7d63bd6fac2.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Cadet",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/495/cadet-068d8b9938da926dce6ea4911d6adfc6.png",
+      course_url: null,
+    },
+    {
+      name: "Level 3 on The Sequel to SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/515/level-3-on-the-sequel-to-sql-c3b7fede9e0268d0aad29c82d48d4a08.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Level 2 on The Sequel to SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/514/level-2-on-the-sequel-to-sql-3b14743cbce827c0dd23ff6329cc821e.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Level 1 on The Sequel to SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/513/level-1-on-the-sequel-to-sql-e1ab0eb7c5188c529a85e0444ae03249.png",
+      course_url: "http://www.codeschool.com/courses/the-sequel-to-sql",
+    },
+    {
+      name: "Scout",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/531/scout-54448d7074a378e1ee249cf9be818f73.png",
+      course_url: null,
+    },
+    {
+      name: "Level 3 on Try SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/489/level-3-on-try-sql-742329712308c860e3b37ab1a46bb78f.png",
+      course_url: "http://www.codeschool.com/courses/try-sql",
+    },
+    {
+      name: "Try SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/490/try-sql-2eeaad2f3a3ad35a5fed7b63563ec9b8.png",
+      course_url: "http://www.codeschool.com/courses/try-sql",
+    },
+    {
+      name: "Level 2 on Try SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/488/level-2-on-try-sql-e66f986bc557122ddc6d9a9f1dbb75d2.png",
+      course_url: "http://www.codeschool.com/courses/try-sql",
+    },
+    {
+      name: "Rookie",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/530/rookie-0fa91bd64fa59be22b5e5600807d1057.png",
+      course_url: null,
+    },
+    {
+      name: "Level 1 on Try SQL",
+      badge:
+        "https://d1ffx7ull4987f.cloudfront.net/images/achievements/large_badge/487/level-1-on-try-sql-68a76aafef4bc08e8b2715d63ee1fa79.png",
+      course_url: "http://www.codeschool.com/courses/try-sql",
+    },
+  ],
+};
